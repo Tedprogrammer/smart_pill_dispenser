7c2d7a3ac9ff0b75a25849b839b381d56e78d03f
Ted Trinh <luonghieutrinh@gmail.com>
2026-05-11 14:38:41 +1000

    Add LCD test script for LiquidCrystal_I2C

---
+++ firmware/component_tests/lcd_test/lcd_test.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Wire.begin(21, 22);
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("LCD TEST OK");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Smart Pill Box");
+}
+
+void loop() {
+}
